da55487c8fe0a3c2b3c18250ea7a758340d331d9
14Berhee <berhee14@gmail.com>
2025-12-28 17:52:47 +0800

    c-deployment

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,27 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
   images: {
-    domains: [
-      'tse3.mm.bing.net',
-      'images.unsplash.com',
-      'www.candere.com',
-      'tse2.mm.bing.net',
-      'd3kinlcl20pxwz.cloudfront.net',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tse3.mm.bing.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.candere.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tse2.mm.bing.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'd3kinlcl20pxwz.cloudfront.net',
+      },
     ],
   },
 };
